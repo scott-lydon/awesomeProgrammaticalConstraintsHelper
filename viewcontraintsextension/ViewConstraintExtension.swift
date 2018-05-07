ed175138ea6ea606e57c4e04f25b8aa03359b98a
scott-lydon <scottlydon18@gmail.com>
2018-05-07 00:12:49 -0700

    Added awesome single view constrainter

--- viewcontraintsextension/ViewConstraintExtension.swift
+++ viewcontraintsextension/ViewConstraintExtension.swift
@@ -8,11 +8,121 @@
 
 import UIKit
 
+protocol Constriant {}
+
 extension UIView {
+    //MARK Group subviews.
     
+    struct ViewWithLeftMargin {
+        var leftMargin: CGFloat
+        var view: UIView
+        var height: CGFloat?
+        var width: CGFloat?
+    }
+    
+    enum Orientation {
+        case horizontal, vertical
+    }
+    
+    enum Centering {
+        case centered, latchToTopMargin
+    }
+    //MARK IN PROGRESS
+//    func subViewsConstraints(topMargin: CGFloat, views: ViewWithLeftMargin.../*, orientation: Orientation = .horizontal*/, centering: Centering = .centered) -> [NSLayoutConstraint] {
+//        var constraints: [[NSLayoutConstraint]] = []
+//        constraints = views.map {
+//            var horizontal1: HorizontalDescriptor
+//            var horizontal2: HorizontalDescriptor
+//            var vertical1: VerticalDescriptor
+//            var vertical2: VerticalDescriptor
+//
+//            if centering == .centered {
+//
+//
+//            } else if centering == .latchToTopMargin {
+//
+//
+//
+//            }
+//
+//
+//
+//
+//            return $0.view.constraints(horizontal: horizontal1, secondHorizontal: horizontal2, vertical: vertical1, secondVertical: vertical2)
+//
+//            }
+//
+//        return constraints.flatMap {$0}
+//    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    //MARK TRY #2
+    
+    enum HorizontalDescriptor: Constriant {
+        case width(CGFloat),
+        distanceToLeft(NSLayoutAnchor<NSLayoutXAxisAnchor>, CGFloat),
+        distanceToRight(NSLayoutAnchor<NSLayoutXAxisAnchor>, CGFloat),
+        centeredWith(UIView)
+    }
+    
+    enum VerticalDescriptor: Constriant {
+        case height(CGFloat),
+        distanceToTop(NSLayoutAnchor<NSLayoutYAxisAnchor>, CGFloat),
+        distanceToBottom(NSLayoutAnchor<NSLayoutYAxisAnchor>, CGFloat),
+        centeredTo(UIView)
+    }
+    
+    func constraints(horizontal: HorizontalDescriptor, secondHorizontal: HorizontalDescriptor, vertical: VerticalDescriptor, secondVertical: VerticalDescriptor, activated shouldActivate: Bool = true) -> [NSLayoutConstraint] {
+        var constraints: [NSLayoutConstraint] = []
+        constraints += [horizontal, secondHorizontal].map {constraint(from: $0)}
+        constraints += [vertical, secondVertical].map {constraint(from: $0)}
+        if shouldActivate {NSLayoutConstraint.activate(constraints)}
+        return constraints
+    }
+    
+    
+    func constraint(from: HorizontalDescriptor) -> NSLayoutConstraint {
+        switch from {
+        case .width(let width):
+            return widthAnchor.constraint(equalToConstant: width)
+        case let .distanceToLeft(anchor, distance):
+            return leadingAnchor.constraint(equalTo: anchor, constant: distance)
+        case let .distanceToRight(anchor, distance):
+            return trailingAnchor.constraint(equalTo: anchor, constant: distance)
+        case .centeredWith(let view):
+            return centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        }
+    }
+    
+    func constraint(from: VerticalDescriptor) -> NSLayoutConstraint {
+        switch from {
+        case .height(let height):
+            return heightAnchor.constraint(equalToConstant: height)
+        case let .distanceToTop(anchor, distance):
+            return topAnchor.constraint(equalTo: anchor, constant: distance)
+        case let .distanceToBottom(anchor, distance):
+            return bottomAnchor.constraint(equalTo: anchor, constant: distance)
+        case .centeredTo(let view):
+            return centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        }
+    }
+    
+    
+    
+    
+    //MARK TRY #1
     enum Flexibility {
         case flexible, notFlexible
     }
+    
+    
     
 //    struct Constraint {
 //        var toView: UIView
